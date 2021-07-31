1dc0e2f545c670d896639ef5b0535ca2d008aa15
Shivani Deshmukh <shivanideshmukh@thekabadiwala.com>
2021-07-31 15:23:33 +0530

    Pickup session map view

--- AdminPortal.Main/Areas/PickupSession/Pages/Ajax/Map.cshtml.cs
+++ AdminPortal.Main/Areas/PickupSession/Pages/Ajax/Map.cshtml.cs
@@ -21,31 +21,64 @@ namespace TKW.AdminPortal.Areas.PickupSession.Pages.Ajax
         }
 
         public List<MarkerDetailViewModel> Map { get; set; }
+        public MapViewModel MapView { get; set; }
         public List<LatLng> PolylinePath { get; set; }
-      
+
+        public List<LatLng> LatlngList { get; set; }
+
+        public List<LatLng> PickupSessionStart { get; set; }
+        public List<LatLng> PickupSessionEnd { get; set; }
+
         public async Task OnGetAsync(int id, CancellationToken cancellationToken)
         {
-            var model = (await _pickupSessionQueries.PickupSessionMapDetailsAsync(id, cancellationToken))
-                .Where(m => m.Latitude != null && m.Longitude != null);
-            Map = model
-                .Select(m => new MarkerDetailViewModel {
-                    Amount = m.Amount,
-                    Id = m.Id,
-                    Latitude = m.Latitude!.Value,
-                    Longitude = m.Longitude!.Value,
-                    LocationType = m.LocationType,
-                    Title = m.Title,
-                    Time = m.Time.ToString("hh:mm tt")
-                })
-                .ToList();
-            
-            PolylinePath = model.Where(m => m.Time != null && m.LocationType != PickupSessionMapViewModel.Type.PendingRequest && m.LocationType != PickupSessionMapViewModel.Type.RescheduledRequest && m.LocationType != PickupSessionMapViewModel.Type.CancelledRequest).OrderBy(m => m.Time).Select(m => new LatLng
+            MapView = await _pickupSessionQueries.PickupSessionMapDetailsAsync(id, cancellationToken);
+
+            Map = MapView.Map.Where(m => m.Latitude != null && m.Longitude != null).Select(m => new MarkerDetailViewModel
+            {
+                Amount = m.Amount,
+                Id = m.Id,
+                Latitude = m.Latitude!.Value,
+                Longitude = m.Longitude!.Value,
+                LocationType = m.LocationType,
+                Title = m.Title,
+                Time = m.Time.ToString("hh:mm tt")
+            }).ToList();
+
+            //var model = (await _pickupSessionQueries.PickupSessionMapDetailsAsync(id, cancellationToken))
+            //    .Where(m => m.Latitude != null && m.Longitude != null);
+            //Map = model
+            //    .Select(m => new MarkerDetailViewModel {
+            //        Amount = m.Amount,
+            //        Id = m.Id,
+            //        Latitude = m.Latitude!.Value,
+            //        Longitude = m.Longitude!.Value,
+            //        LocationType = m.LocationType,
+            //        Title = m.Title,
+            //        Time = m.Time.ToString("hh:mm tt")
+            //    })
+            //    .ToList();
+
+            //var pickupSessionStart = new LatLng { Lat = MapView.StartLatitude.Value , Lng = MapView.StartLongitude.Value };
+            //var pickupSessionEnd = new LatLng { Lat = MapView.EndLatitude.Value, Lng = MapView.EndLongitude.Value };
+
+
+
+            LatlngList = MapView.Map.Where(m => m.Time != null && m.LocationType != PickupSessionMapViewModel.Type.PendingRequest && m.LocationType != PickupSessionMapViewModel.Type.RescheduledRequest && m.LocationType != PickupSessionMapViewModel.Type.CancelledRequest).OrderBy(m => m.Time).Select(m => new LatLng
             {
                 Lat = m.Latitude!.Value,
                 Lng = m.Longitude!.Value
             }).ToList();
 
-            
+            List<LatLng> pickupSessionStart = new List<LatLng>();
+            pickupSessionStart.Add(new LatLng { Lat = MapView.StartLatitude.Value, Lng = MapView.StartLongitude.Value });
+
+            List<LatLng> pickupSessionEnd = new List<LatLng>();
+            pickupSessionEnd.Add(new LatLng { Lat = MapView.EndLatitude.Value, Lng = MapView.EndLongitude.Value });
+
+            PolylinePath  = LatlngList.Concat(pickupSessionStart).Concat(pickupSessionEnd).ToList();
+
+
+
         }
     }
     public class LatLng
