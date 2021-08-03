493299687743e1e3227edb726fe9c0280b68a125
Shivani Deshmukh <shivanideshmukh@thekabadiwala.com>
2021-08-03 19:04:00 +0530

    Add pickup session marker

--- AdminPortal.Main/Areas/PickupSession/Pages/Ajax/Map.cshtml.cs
+++ AdminPortal.Main/Areas/PickupSession/Pages/Ajax/Map.cshtml.cs
@@ -20,20 +20,16 @@ namespace TKW.AdminPortal.Areas.PickupSession.Pages.Ajax
             _pickupSessionQueries = pickupSessionQueries;
         }
 
-        public List<MarkerDetailViewModel> Map { get; set; }
+        //public List<MarkerDetailViewModel> Map { get; set; }
+        public List<MarkerDetailViewModel> MapList { get; set; }
         public MapViewModel MapView { get; set; }
-        public List<LatLng> PolylinePath { get; set; }
-
-        public List<LatLng> LatlngList { get; set; }
-
-        public List<LatLng> PickupSessionStart { get; set; }
-        public List<LatLng> PickupSessionEnd { get; set; }
+        public List<LatLng> PolylinePathList { get; set; }
 
         public async Task OnGetAsync(int id, CancellationToken cancellationToken)
         {
             MapView = await _pickupSessionQueries.PickupSessionMapDetailsAsync(id, cancellationToken);
 
-            Map = MapView.Map.Where(m => m.Latitude != null && m.Longitude != null).Select(m => new MarkerDetailViewModel
+            var Map = MapView.Map.Where(m => m.Latitude != null && m.Longitude != null).Select(m => new MarkerDetailViewModel
             {
                 Amount = m.Amount,
                 Id = m.Id,
@@ -44,30 +40,15 @@ namespace TKW.AdminPortal.Areas.PickupSession.Pages.Ajax
                 Time = m.Time.ToString("hh:mm tt")
             }).ToList();
 
-            //var model = (await _pickupSessionQueries.PickupSessionMapDetailsAsync(id, cancellationToken))
-            //    .Where(m => m.Latitude != null && m.Longitude != null);
-            //Map = model
-            //    .Select(m => new MarkerDetailViewModel {
-            //        Amount = m.Amount,
-            //        Id = m.Id,
-            //        Latitude = m.Latitude!.Value,
-            //        Longitude = m.Longitude!.Value,
-            //        LocationType = m.LocationType,
-            //        Title = m.Title,
-            //        Time = m.Time.ToString("hh:mm tt")
-            //    })
-            //    .ToList();
+            List<MarkerDetailViewModel> markerDetailViewModel = new List<MarkerDetailViewModel>();
+            markerDetailViewModel.Add(new MarkerDetailViewModel { Latitude = MapView.StartLatitude!.Value, Longitude = MapView.StartLongitude!.Value,LocationType = PickupSessionMapViewModel.Type.PickupSessionStart });
 
-            //var pickupSessionStart = new LatLng { Lat = MapView.StartLatitude.Value , Lng = MapView.StartLongitude.Value };
-            //var pickupSessionEnd = new LatLng { Lat = MapView.EndLatitude.Value, Lng = MapView.EndLongitude.Value };
+            List<MarkerDetailViewModel> markerDetailView = new List<MarkerDetailViewModel>();
+            markerDetailView.Add(new MarkerDetailViewModel { Latitude = MapView.EndLatitude!.Value, Longitude = MapView.EndLongitude!.Value,LocationType = PickupSessionMapViewModel.Type.PickupSessionEnd });
+
+            MapList = Map.Concat(markerDetailView).Concat(markerDetailViewModel).ToList();
 
 
-
-            LatlngList = MapView.Map.Where(m => m.Time != null && m.LocationType != PickupSessionMapViewModel.Type.PendingRequest && m.LocationType != PickupSessionMapViewModel.Type.RescheduledRequest && m.LocationType != PickupSessionMapViewModel.Type.CancelledRequest).OrderBy(m => m.Time).Select(m => new LatLng
-            {
-                Lat = m.Latitude!.Value,
-                Lng = m.Longitude!.Value
-            }).ToList();
 
             List<LatLng> pickupSessionStart = new List<LatLng>();
             pickupSessionStart.Add(new LatLng { Lat = MapView.StartLatitude.Value, Lng = MapView.StartLongitude.Value });
@@ -75,9 +56,13 @@ namespace TKW.AdminPortal.Areas.PickupSession.Pages.Ajax
             List<LatLng> pickupSessionEnd = new List<LatLng>();
             pickupSessionEnd.Add(new LatLng { Lat = MapView.EndLatitude.Value, Lng = MapView.EndLongitude.Value });
 
-            PolylinePath  = LatlngList.Concat(pickupSessionStart).Concat(pickupSessionEnd).ToList();
+            var PolylinePath = MapView.Map.Where(m => m.Time != null && m.LocationType != PickupSessionMapViewModel.Type.PendingRequest && m.LocationType != PickupSessionMapViewModel.Type.RescheduledRequest && m.LocationType != PickupSessionMapViewModel.Type.CancelledRequest).OrderBy(m => m.Time).Select(m => new LatLng
+            {
+                Lat = m.Latitude!.Value,
+                Lng = m.Longitude!.Value
+            }).ToList();
 
-
+            PolylinePathList = PolylinePath.Concat(pickupSessionStart).Concat(pickupSessionEnd).ToList();
 
         }
     }
@@ -91,12 +76,12 @@ namespace TKW.AdminPortal.Areas.PickupSession.Pages.Ajax
     }
     public class MarkerDetailViewModel
     {
-        public long Id { get; set; }
+        public long? Id { get; set; }
         public PickupSessionMapViewModel.Type LocationType { get; set; }
         public decimal? Amount { get; set; }
-        public string Title { get; set; }
+        public string? Title { get; set; }
         public decimal Latitude { get; set; }
         public decimal Longitude { get; set; }
-        public string Time { get; set; }
+        public string? Time { get; set; }
     }
 }
