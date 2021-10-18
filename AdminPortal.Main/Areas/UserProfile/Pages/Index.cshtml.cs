c3b48223ce007ed58686a595a4375281fce87ada
Shailendra98 <ssinghlodhi08@gmail.com>
2021-10-18 18:05:34 +0530

    Fix Payment Methods

--- AdminPortal.Main/Areas/UserProfile/Pages/Index.cshtml.cs
+++ AdminPortal.Main/Areas/UserProfile/Pages/Index.cshtml.cs
@@ -1,16 +1,25 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading;
+using System.Threading.Tasks;
+using TKW.Queries.DTOs.Account;
 
 namespace TKW.AdminPortal.Areas.UserProfile.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+       
+        [BindProperty(SupportsGet = true)]
+        public int? PageNo { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public int? PageSize { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public UserFilterSortModel Filter { get; set; }
+        public async Task OnGetAsync(CancellationToken cancellationToken)
         {
+
         }
     }
 }
