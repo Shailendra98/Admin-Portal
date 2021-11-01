dc05495d9997c8fc3ae228f17ccce967e98e3192
Shailendra98 <ssinghlodhi08@gmail.com>
2021-11-01 18:18:29 +0530

    Add Details pAge in User

--- AdminPortal.Main/Areas/UserProfile/Pages/Ajax/Modal/DeleteAddress.cshtml.cs
+++ AdminPortal.Main/Areas/UserProfile/Pages/Ajax/Modal/DeleteAddress.cshtml.cs
@@ -8,20 +8,22 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using TKW.Queries.DTOs.Account;
 using TKW.Queries.Interfaces;
 
-namespace TKW.AdminPortal.Areas.UserProfile.Pages.Ajax
+namespace TKW.AdminPortal.Areas.UserProfile.Pages.Ajax.Modal
 {
-    public class UserProfileModel : PageModel
+    public class DeleteAddressModel : PageModel
     {
         private IUserQueries _userQueries;
         private IUserAddressQueries _userAddressQueries;
 
-        public UserProfileModel(IUserQueries userQueries, IUserAddressQueries userAddressQueries)
+        public DeleteAddressModel(IUserQueries userQueries, IUserAddressQueries userAddressQueries)
         {
             _userQueries = userQueries;
             _userAddressQueries = userAddressQueries;
 
         }
 
+        [BindProperty(SupportsGet = true)]
+        public string? Tab { get; set; }
 
         public UserAddressModel? UserAddress { get; set; }
 
