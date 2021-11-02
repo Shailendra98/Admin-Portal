97d2d279a4f0fe7be02aa94df9f27bbac25dc61f
Shailendra98 <ssinghlodhi08@gmail.com>
2021-11-02 19:08:24 +0530

    Fix address and payment

--- AdminPortal.Main/Areas/UserProfile/Pages/Details.cshtml.cs
+++ AdminPortal.Main/Areas/UserProfile/Pages/Details.cshtml.cs
@@ -1,16 +1,31 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading;
+using System.Threading.Tasks;
+using TKW.Queries.DTOs.Account;
+using TKW.Queries.Interfaces;
 
 namespace TKW.AdminPortal.Areas.UserProfile.Pages
 {
     public class DetailsModel : PageModel
     {
-        public void OnGet()
+        private IUserQueries _userQueries;
+        private IUserAddressQueries _userAddressQueries;
+
+        public DetailsModel(IUserQueries userQueries, IUserAddressQueries userAddressQueries)
         {
+            _userQueries = userQueries;
+            _userAddressQueries = userAddressQueries;
+        }
+
+
+        public UserModel? Users { get; set; }
+        public UserAddressModel? Address{get; set;}
+
+        public async Task OnGetAsync(int id, CancellationToken cancellationToken)
+        {
+            Users = await _userQueries.UserByIdAsync(id, cancellationToken);
+            Address = await _userAddressQueries.UserAddressByIdAsync(id, cancellationToken);
+
         }
     }
 }
