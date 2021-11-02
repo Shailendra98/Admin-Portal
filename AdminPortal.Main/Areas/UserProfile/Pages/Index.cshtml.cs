b06ccfb4105197f33c99cc1df52df4acab8425ba
Shailendra98 <ssinghlodhi08@gmail.com>
2021-11-02 12:04:13 +0530

    Replace All contain And Fix According user

--- AdminPortal.Main/Areas/UserProfile/Pages/Index.cshtml.cs
+++ AdminPortal.Main/Areas/UserProfile/Pages/Index.cshtml.cs
@@ -1,14 +1,19 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using TKW.ApplicationCore.Contexts.AccountContext.Aggregates;
+using TKW.ApplicationCore.SeedWorks;
 using TKW.Queries.DTOs.Account;
+
 
 namespace TKW.AdminPortal.Areas.UserProfile.Pages
 {
     public class IndexModel : PageModel
     {
-       
         [BindProperty(SupportsGet = true)]
         public int? PageNo { get; set; }
 
@@ -17,9 +22,29 @@ namespace TKW.AdminPortal.Areas.UserProfile.Pages
 
         [BindProperty(SupportsGet = true)]
         public UserFilterSortModel Filter { get; set; }
+        public List<AddressType> AddressTypes { get; set; }
         public async Task OnGetAsync(CancellationToken cancellationToken)
         {
+            AddressTypes = Enumeration.GetAll<AddressType>().ToList();
+        }
 
+        public enum DateTypeOption
+        {
+            [Display(Name = "Joining Date")]
+            JoiningDate,
+            [Display(Name = "Last Request Handle Date")]
+            LastRequestHandledDate,
+        }
+        public enum SortByOption
+        {
+            [Display(Name = "Joining Date")]
+            JoiningDate,
+            [Display(Name = "Name")]
+            Name,
+            [Display(Name = "Request Count")]
+            RequestCount,
+            [Display(Name = "LastRequestHandleDate")]
+            LastRequestHandleDate,
         }
     }
 }
